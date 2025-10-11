5438f4892c8d8694f711b73c44f7261c226b2323
Piahn <owijaya131@gmail.com>
2025-10-11 16:37:27 +0700

    repos: frontend structure folder and deps

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,44 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Kantin Pelajar - HomePage Makanan Kantin Sekolah Tanpa Antre",
+  description:
+    "Kantin Pelajar adalah solusi pre-order makanan di kantin sekolah. Pesan jajanan, minuman, & ATK favoritmu secara online dan hindari antrean panjang. Lebih hemat waktu!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
